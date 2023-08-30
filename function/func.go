eeb8b2ece220d5bd8d2f7367c14415f67fe67e41
Ari Muhammad Rizal <arimrizal94@gmail.com>
2023-08-31 02:19:35 +0700

    ft:func concat

--- function/func.go
+++ function/func.go
@@ -14,6 +14,17 @@ var sum = func(x ...int) int {
 	return res
 }
 
+var concat = func(s ...string) string {
+	if len(s) <= 1 {
+		return fmt.Sprintln("kasihlah 2 kata biar bisa digabungkan")
+	}
+	for i := 1; i < len(s); i++ {
+		s[0] += s[i]
+	}
+	return s[0]
+}
+
 func main() {
 	fmt.Println(sum(1, 2, 3, 4))
+	fmt.Println(concat("hello", "world"))
 }
