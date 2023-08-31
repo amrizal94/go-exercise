1d7e102cc5cb74c83640a38058780454edbde3e0
Ari Muhammad Rizal <arimrizal94@gmail.com>
2023-08-31 15:23:02 +0700

    add:tambahan struck format uang

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module exercise
 
 go 1.21.0
+
+require github.com/go-playground/locales v0.14.1 // indirect
--- struck/struck.go
+++ struck/struck.go
@@ -1,6 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/go-playground/locales/en_US"
+)
+
+func main() {
+	l := en_US.New()
+	byson := motor{
+		cc:    150,
+		merek: "Yamaha Byson",
+		banDepan: ban{
+			lebar: 80,
+		},
+	}
+	var myMotor []motor
+	byson.banBelakang.lebar = 90
+	byson.banBelakang.aspectRatio = 100
+	myMotor = append(myMotor, byson)
+	beat := motor{
+		cc:    110,
+		merek: "Honda Beat",
+	}
+	beat.banDepan.lebar = 70
+	beat.banBelakang.lebar = 70
+	myMotor = append(myMotor, beat)
+	for _, m := range myMotor {
+		pajak(&m)
+		fmt.Printf("motor %s kenak pajak sebesar %v\n", m.merek, m.pajak)
+		switch {
+		case cekBan(m):
+			fmt.Printf("ban depan %v cocok dengan ban belakang yg lebih besar\n", m.merek)
+		default:
+			fmt.Printf("ganti ban belakang %v dengan lebih besar\n", m.merek)
+		}
+	}
+	hpku := hp{
+		merek: "apple",
+		speck: speck{
+			processor: "M1",
+			gpu:       "7-core",
+		},
+		harga: 1000000,
+	}
+
+	fmt.Println(hpku)
+	fmt.Printf("->Detail HPku<-\nmerek: %v\nprocessor: %v\ngpu: %v\nharga: Rp. %v\n", hpku.merek, hpku.processor, hpku.gpu, l.FmtNumber(float64(hpku.harga), 2))
+	myIpad := struct {
+		version string
+		harga   int
+	}{
+		version: "Ipad Air 5",
+		harga:   5000000,
+	}
+	fmt.Println(myIpad)
+	s := fmt.Sprintf("%.2f", 11211.12112)
+	log.Println(s)
+	f := strconv.Quote(`"Fran & Freddie's Diner	☺"`)
+	log.Println(f)
+	var f64 = 78.12
+
+	// Percent
+	fmt.Println(l.FmtPercent(f64, 2))
+	// Number
+	fmt.Println(l.FmtNumber(float64(hpku.harga), 2))
+
+}
 
 type motor struct {
 	merek       string
@@ -43,52 +111,4 @@ type hp struct {
 	speck
 	merek string
 	harga int
-}
-
-func main() {
-	byson := motor{
-		cc:    150,
-		merek: "Yamaha Byson",
-		banDepan: ban{
-			lebar: 80,
-		},
-	}
-	var myMotor []motor
-	byson.banBelakang.lebar = 90
-	byson.banBelakang.aspectRatio = 100
-	myMotor = append(myMotor, byson)
-	beat := motor{
-		cc:    110,
-		merek: "Honda Beat",
-	}
-	beat.banDepan.lebar = 70
-	beat.banBelakang.lebar = 70
-	myMotor = append(myMotor, beat)
-	for _, m := range myMotor {
-		pajak(&m)
-		fmt.Printf("motor %s kenak pajak sebesar %v\n", m.merek, m.pajak)
-		switch {
-		case cekBan(m):
-			fmt.Printf("ban depan %v cocok dengan ban belakang yg lebih besar\n", m.merek)
-		default:
-			fmt.Printf("ganti ban belakang %v dengan lebih besar\n", m.merek)
-		}
-	}
-	hpku := hp{
-		merek: "apple",
-		speck: speck{
-			processor: "M1",
-			gpu:       "7-core",
-		},
-		harga: 1000000,
-	}
-	fmt.Println(hpku)
-	myIpad := struct {
-		version string
-		harga   int
-	}{
-		version: "Ipad Air 5",
-		harga:   5000000,
-	}
-	fmt.Println(myIpad)
 }
