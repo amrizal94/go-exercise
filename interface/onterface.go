570ce8044dc4f733688ca7bf0e902c5550edc26c
Ari Muhammad Rizal <=>
2023-09-02 10:33:39 +0700

    add keliling

--- interface/onterface.go
+++ interface/onterface.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-type shape interface {
+type Shape interface {
 	luas() float32
 	keliling() float32
 }
@@ -30,11 +30,10 @@ func (s segitiga) luas() float32 {
 	return float32(s.alas * s.tinggi * 1 / 2)
 }
 
-func (s segitiga) keliling() float32 {
-	return float32(s.alas + (s.sisiMiring * 2))
-}
-func (s *segitiga) pytagoras() {
+func (s *segitiga) keliling() float32 {
 	s.sisiMiring = math.Sqrt(s.alas*s.alas + s.tinggi*s.tinggi)
+
+	return float32(s.alas + (s.sisiMiring * 2))
 }
 
 func (pP persegiPanjang) luas() float32 {
@@ -61,34 +60,31 @@ func (l lingkaran) keliling() float32 {
 	return 2 * math.Pi * l.jari2
 }
 
-var hitung = func(s shape) float32 {
+var hitungLuas = func(s Shape) float32 {
 	return s.luas()
 }
 
-var hitungKeliling = func(s shape) float32 {
-
+var hitungKeliling = func(s Shape) float32 {
 	return s.keliling()
 }
 
 func main() {
-	l := lingkaran{
-		jari2: 5,
-	}
-	fmt.Printf("Diket: jari²: %v cm, maka luas lingkaran: %v cm²\n", l.jari2, hitung(l))
+
+	l := &lingkaran{jari2: 5}
+	fmt.Printf("Diket: jari²: %v cm, maka luas: %v cm² dan keliling lingkaran: %v cm\n", hitungLuas(l), l.luas(), l.keliling())
 
 	pPanjang := persegiPanjang{
 		panjang: 17,
 		lebar:   7,
 	}
-	fmt.Printf("Diket: panjang: %v cm dan lebar: %v cm, maka luas persegi panjang: %v cm²\n", pPanjang.panjang, pPanjang.lebar, hitung(pPanjang))
+	fmt.Printf("Diket: panjang: %v cm dan lebar: %v cm, maka luas: %v cm² dan keliling persegi panjang: %v cm\n", pPanjang.panjang, pPanjang.lebar, pPanjang.luas(), pPanjang.keliling())
 	p := persegi{sisi: 7}
-	fmt.Printf("Diket: sisi: %v cm, maka luas persegi: %v cm²\n", p.sisi, hitung(p))
-	s := segitiga{
+	fmt.Printf("Diket: sisi: %v cm, maka luas persegi: %v cm²\n", p.sisi, p.luas())
+	s := &segitiga{
 		alas:   8,
 		tinggi: 6,
 	}
-	fmt.Printf("Diket: alas: %v cm dan tinggi: %v cm, maka luas segitiga: %v cm²\n", s.alas, s.tinggi, hitung(s))
-	s.pytagoras()
+	fmt.Printf("Diket: alas: %v cm dan tinggi: %v cm, maka luas segitiga: %v cm²\n", s.alas, s.tinggi, s.luas())
 	fmt.Printf("Dicari sisi miring mengunakan pytagoras: %v cm, maka keliling segitiga: %v cm\n", s.sisiMiring, hitungKeliling(s))
 
 	var x interface{} = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
